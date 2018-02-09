5400f2bf1a380867369522337c01a673d04659e0
francois <francois@cybergraph.fr>
2018-02-09 10:30:49 +0100

    test GITHUB

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input v-model="msg">
+    <input v-model="msg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Hello app de test GIT 2'
+    }
+  }
+}
+</script>
